b18c2a4ad24b0f19dbe25e3d0eb7a06103e3caec
Wami22 <129443465+Waman22@users.noreply.github.com>
2025-07-19 12:57:49 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,5 +15,9 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the MERN TODO APP');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
